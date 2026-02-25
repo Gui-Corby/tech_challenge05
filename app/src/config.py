8a1710785018fa87f225f41ed9c9b455b9578853
Guilherme Corby <96598840+Gui-Corby@users.noreply.github.com>
2026-02-25 13:22:29 -0300

    making feature_engineering

---
+++ app/src/config.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+DF_2022 = pd.read_excel("data/excel_file.xlsx", sheet_name="PEDE2022")
+DF_2023 = pd.read_excel("data/excel_file.xlsx", sheet_name='PEDE2023')
+DF_2024 = pd.read_excel("data/excel_file.xlsx", sheet_name="PEDE2024")
